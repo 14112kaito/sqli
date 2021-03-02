9799e4bc7adef77b817d55929a8f84e7c8f03bb2
Mohamed Maghnouj <momaghnouj@SMB.SQLI.COM>
2021-03-02 17:40:11 +0100

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Dr. Strange'},
+  {id: 12, name: 'Captain America'},
+  {id: 13, name: 'Iron Man'},
+  {id: 14, name: 'Blach Widow'},
+  {id: 15, name: 'Thor', src: '../assets/img/thor.png'},
+  {id: 16, name: 'Hulk'},
+  {id: 17, name: 'Ant-Man'},
+  {id: 18, name: 'Hawkeye'},
+  {id: 19, name: 'Nick'},
+  {id: 20, name: 'The-Wasp'},
+];
